dbfb074f2d7b22e0a1a4c977ba36eb49f2b9112a
Monster0K <158990851+Monster0K@users.noreply.github.com>
2024-02-05 09:35:56 +0000

    datatypes

---
+++ 01_basics-js/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 424253
+let accountEmail = "navin@gmail.com"
+var accountPass = "123456"
+let accountName = "monster"
+let account;
+
+console.log(accountId);
+console.table([accountId, accountEmail  ,accountName , accountPass, account])
---
+++ 01_basics-js/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert('messege' , c) // is not work in page it is only work in console
+
+const number = 543 //number
+const text = "string" // string
+
+console.log(typeof undefined);// undefine is string in javascript
+console.log(typeof null);   // null is a object in javascript
