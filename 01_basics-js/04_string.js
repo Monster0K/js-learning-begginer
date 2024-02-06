1ad41aa11d2314a4b4894afcd3fae0bdc0cc4f74
Monster0K <158990851+Monster0K@users.noreply.github.com>
2024-02-06 10:31:51 +0000

    Math and num

--- 01_basics-js/04_string.js
+++ 01_basics-js/04_string.js
@@ -2,10 +2,15 @@ let name = "monster"
 
 let repoCount = 50
 
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('KiyanshWeb')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
+
+const url = "https://Monster.com/navin%20chauhan"
+
+console.log(url.replace('%20', '@'));
+console.log(url.includes('hello'));
