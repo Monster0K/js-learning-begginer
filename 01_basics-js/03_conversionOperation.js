ec51c50b1b98561a1f20242d629a13ca61c79712
Monster0K <158990851+Monster0K@users.noreply.github.com>
2024-02-06 07:13:01 +0000

    String chages

--- 01_basics-js/03_conversionOperation.js
+++ 01_basics-js/03_conversionOperation.js
@@ -4,9 +4,12 @@ console.log(typeof score);
 console.log(typeof (score));
 
 let valNumber = Number(score)
-console.log(typeof valNumber);
-console.log(valNumber); // actual it is not a number its a NaN because of it a converion of string
+// console.log(typeof valNumber);
+// console.log(valNumber); // actual it is not a number its a NaN because of it a converion of string
 
 // 33 => 33
 // "33a" => NaN
 // true => 1
+let number = 34;
+// console.log(number++);
+// console.log(++number);
---
+++ 01_basics-js/04_string.js
@@ -0,0 +1,11 @@
+let name = "monster"
+
+let repoCount = 50
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('KiyanshWeb')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
